51fe463a70a866a320a57db132d0ce89b747a1a6
anzueto <jose@C4-1152.local>
2020-01-02 14:10:33 -0500

    added state and render tittle of box

---
+++ front_end/src/components/Expenses/Expenses.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class expensesData extends Component {
+    state = {
+        expenses: [],
+        name: '',
+        amount: 0
+    }
+
+    render() {
+        <div className="expenses-data-container">
+            <h3>All Expenses</h3>
+        </div>
+    }
+
+}
+
+export default expensesData;
