c6b88af1a79d72e7670cc7f896f06beba6f6e955
anzueto <jose@C4-1152.local>
2020-01-02 14:23:52 -0500

    added display data and delete button

--- front_end/src/components/Expenses/Expenses.js
+++ front_end/src/components/Expenses/Expenses.js
@@ -10,6 +10,29 @@ class expensesData extends Component {
     render() {
         <div className="expenses-data-container">
             <h3>All Expenses</h3>
+
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">Name</th>
+                        <th scope="col">Amount</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.state.expenses.map((expenses, index) => {
+                        return (
+                            <tr>
+                                <th scope="row">{this.state.name}</th>
+                                <td>{this.state.amount}</td>
+                            </tr>
+
+                            <tr>
+                                <td onClick={(e) => this.deleteExpenses()}><span className="delete-btn">Delete All expenses</span> </td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
         </div>
     }
 
